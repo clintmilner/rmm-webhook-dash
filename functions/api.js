ee09761125ec87261adae15ac5168fb59e6a4e51
Clint Milner <cmilner@datto.com>
2022-02-03 17:00:41 +0000

    trying to fetch data

--- functions/api.js
+++ functions/api.js
@@ -1,31 +1,11 @@
-const fakeData = [{
-    "hostname": "🌮 CLINT-PC",
-    "deviceId": "247393",
-    "siteId": "2590",
-    "siteName": "Colorado",
-    "msg": "Process 'firefox' isRunning",
-    "category": "PROCESS",
-    "priority": "low",
-    "date": 1643886943346,
-    "_id": "f153772630dc4b7b859c1d0ac9cfd213"
-}, {
-    "hostname": "CLINT-PC",
-    "deviceId": "247393",
-    "siteId": "2590",
-    "siteName": "Colorado",
-    "msg": "Process 'firefox' isRunning",
-    "category": "PROCESS",
-    "priority": "low",
-    "date": 1643887723204,
-    "_id": "0af88291286c43f4a3a39ebb26308075"
-}]
 const diskdb = require('diskdb')
 const dbPath = './src/db'
 
 exports.handler = async (event, context) => {
-    diskdb.connect(dbPath, ['alerts']);
+    const data = diskdb.connect(dbPath, ['alerts'])
+    const alerts = await data.alerts.find()
     return {
         statusCode: 200,
-        body: JSON.stringify(fakeData)
+        body: JSON.stringify(alerts)
     }
 }
