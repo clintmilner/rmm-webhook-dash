1b952e88cdd16e26f1cae0481b49dba6bc5cbef4
Clint Milner <cmilner@datto.com>
2022-02-03 16:45:33 +0000

    pulling data back from api

--- functions/api.js
+++ functions/api.js
@@ -1,6 +1,12 @@
+const diskdb = require('diskdb')
+const dbPath = './src/db'
+
+
 exports.handler = async (event, context) => {
+    const data = diskdb.connect(dbPath, ['alerts'])
+    const alerts = data.alerts.find()
     return {
         statusCode: 200,
-        body: JSON.stringify({message: 'api has been hit!'})
+        body: JSON.stringify(alerts)
     }
 }
--- src/App.js
+++ src/App.js
@@ -1,38 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 import {useEffect, useState} from "react";
 import axios from 'axios'
 
-function App() {
+const App = () => {
   const [alerts, setAlerts] = useState([])
 
   const fetchData = async () => {
     const results = await axios('/.netlify/functions/api')
     console.info(results)
 
-    setAlerts(results?.data?.message)
+    setAlerts(results?.data)
   }
 
 
   useEffect(() => {
     void fetchData()
   },[])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {alerts}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="dashboard">
+        {
+          alerts?.map(({hostname, siteName}, idx) => (<p key={idx}>{hostname} - {siteName}</p>))
+        }
     </div>
   );
 }
