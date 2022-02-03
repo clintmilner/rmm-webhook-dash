d7752196c13997e630bbf4f6bbfec2697fbe916c
Clint Milner <cmilner@datto.com>
2022-02-03 16:50:17 +0000

    api not working on remote server

--- functions/api.js
+++ functions/api.js
@@ -1,12 +1,8 @@
-const diskdb = require('diskdb')
-const dbPath = './src/db'
-
+const fakeData = [{"hostname":"🌮 CLINT-PC","deviceId":"247393","siteId":"2590","siteName":"Colorado","msg":"Process 'firefox' isRunning","category":"PROCESS","priority":"low","date":1643886943346,"_id":"f153772630dc4b7b859c1d0ac9cfd213"},{"hostname":"CLINT-PC","deviceId":"247393","siteId":"2590","siteName":"Colorado","msg":"Process 'firefox' isRunning","category":"PROCESS","priority":"low","date":1643887723204,"_id":"0af88291286c43f4a3a39ebb26308075"}]
 
 exports.handler = async (event, context) => {
-    const data = diskdb.connect(dbPath, ['alerts'])
-    const alerts = data.alerts.find()
     return {
         statusCode: 200,
-        body: JSON.stringify(alerts)
+        body: JSON.stringify(fakeData)
     }
 }
