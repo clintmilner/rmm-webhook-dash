9f40fcbffcc9efe4aac50c14b1c0cecade4cf64c
Clint Milner <cmilner@datto.com>
2022-02-03 09:26:55 +0000

    only accepting data from our site but sending 200 on test webhook request

--- src/api.js
+++ src/api.js
@@ -5,43 +5,42 @@ const diskdb = require('diskdb')
 const cors = require('cors')
 const app = express()
 const router = express.Router()
+require('dotenv').config()
 
 const PORT = process.env.PORT || 3333
+
 const dbPath = './src/db'
-
-app.use(cors())
-
-// const allowedDomains = ['http://localhost','https://rmmbuilds-review-fixwebhooks.sandbox.rmm.datto.com','https://sandboxrmm.centrastage.net','https://clintmilner.github.io/rmm-webhook-dash']
-// const corsOptions = {
-//     origin:  (origin, callback) => {
-//         if (allowedDomains.includes(origin)) {
-//             callback(null, true)
-//         } else {
-//             callback(new Error('Not allowed by CORS'))
-//         }
-//     }
-// }
-
-
 diskdb.connect(dbPath, ['alerts']);
 
 app.use(cors())
 app.use(bodyParser.json())
+
 router.post("/hook",  (req, res) => {
-    const {hostname, deviceId, msg, category} = req?.body
+    const {hostname, deviceId, siteId} = req?.body
 
     // validate the data is good
-    if(!hostname || !deviceId){
-        res.status(400).end()
+    if(!hostname || !deviceId || !siteId){
+        return res.status(400).end()
     }
 
+    // successful webhook test
+    if(siteId === '[site_id]') {
+        return res.status(200).end()
+    }
+
+    // only accepting data from our site
+    if(siteId !== process.env.ALLOWED_SITE_ID) {
+        return res.status(400).end()
+    }
+
+
     const now = Date.now()
-    const alert = {hostname, deviceId, msg, category, date:now}
+    const alert = {...req?.body, date:now}
 
     // save the data somewhere
     diskdb.connect(dbPath, ['alerts']);
     diskdb.alerts.save(alert);
-    res.status(200).end()
+    return res.status(200).end()
 })
 
 router.get('/', (req, res) => {
