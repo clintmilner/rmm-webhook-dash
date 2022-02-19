5b1e6fbd3c6eb50d6e4292830462a1dc5de6c3b3
Clint Milner <cmilner@datto.com>
2022-02-19 12:22:44 +0000

    trying a few things

--- functions/api.js
+++ functions/api.js
@@ -1,20 +1,37 @@
-const diskdb = require("diskdb");
 
-exports.handler = async (event, context) => {
-    try {
-        const diskdb = require('diskdb')
-        const dbPath = './functions/db'
-        const data = diskdb.connect(dbPath, ['alerts'])
-        const alerts = await data.alerts.find()
-
-        return {
-            statusCode: 200,
-            body: JSON.stringify(alerts)
-        }
-    } catch (e) {
-        return {
-            statusCode: 501,
-            body: JSON.stringify(e)
-        }
-    }
+exports.handler = (event, context) => {
+    const faunadb = require('faunadb')
+    const q = faunadb.querynom
+    console.log('Function `todo-read-all` invoked')
+    /* configure faunaDB Client with our secret */
+    const client = new faunadb.Client({
+        secret: process.env.FAUNADB_SERVER_SECRET
+    })
+    console.log('fauna db client init')
+    console.log('=========')
+    console.log('=========')
+    console.log('=========')
+    return client.query(q.Paginate(Collection('alerts')))
+        .then((response) => {
+            const todoRefs = response.data
+            console.log('Todo refs', todoRefs)
+            console.log(`${todoRefs.length} todos found`)
+            // create new query out of todo refs. http://bit.ly/2LG3MLg
+            const getAllTodoDataQuery = todoRefs.map((ref) => {
+                return q.Get(ref)
+            })
+            // then query the refs
+            return client.query(getAllTodoDataQuery).then((ret) => {
+                return {
+                    statusCode: 200,
+                    body: JSON.stringify(ret)
+                }
+            })
+        }).catch((error) => {
+            console.log('error', error)
+            return {
+                statusCode: 400,
+                body: JSON.stringify(error)
+            }
+        })
 }
--- src/App.js
+++ src/App.js
@@ -3,30 +3,30 @@ import {useEffect, useState} from "react";
 import axios from 'axios'
 
 const App = () => {
-  const [alerts, setAlerts] = useState([])
+    const [alerts, setAlerts] = useState([])
 
-  const fetchData = async () => {
-    try {
-      const results = await axios('/.netlify/functions/api')
-      console.info(results)
+    const fetchData = async () => {
+        try {
+            const results = await axios('/.netlify/functions/api')
+            console.info(results)
 
-      setAlerts(results?.data)
-    } catch (e) {
-      console.error('ui error', e)
+            setAlerts(results?.data)
+        } catch (e) {
+            console.error('ui error', e)
+        }
     }
-  }
 
 
-  useEffect(() => {
-    void fetchData()
-  },[])
+    useEffect(() => {
+        void fetchData()
+    }, [])
 
 
-  return (
-    <div className="dashboard">
-      <pre >{JSON.stringify(alerts)}</pre>
-    </div>
-  );
+    return (
+        <div className="dashboard">
+            <pre>{JSON.stringify(alerts)}</pre>
+        </div>
+    );
 }
 
 export default App;
