2bfd3bd235436df5ed409098234fde06a842ea7f
Clint Milner <cmilner@datto.com>
2022-02-19 12:52:58 +0000

    db hooked up

--- functions/api.js
+++ functions/api.js
@@ -1,37 +1,33 @@
-
 exports.handler = (event, context) => {
-    const faunadb = require('faunadb')
-    const q = faunadb.querynom
-    console.log('Function `todo-read-all` invoked')
-    /* configure faunaDB Client with our secret */
-    const client = new faunadb.Client({
-        secret: process.env.FAUNADB_SERVER_SECRET
+  const faunadb = require('faunadb')
+  const q = faunadb.query
+  const client = new faunadb.Client({
+    secret: process.env.FAUNADB_SERVER_SECRET,
+    domain: 'db.eu.fauna.com',
+    port: 443,
+    scheme: 'https',
+  })
+
+  return client
+    .query(q.Get(q.Collection('alerts')))
+    .then(ret => {
+      console.log('anything?', ret)
+      return {
+        statusCode: 200,
+        body: JSON.stringify(ret),
+      }
     })
-    console.log('fauna db client init')
-    console.log('=========')
-    console.log('=========')
-    console.log('=========')
-    return client.query(q.Paginate(Collection('alerts')))
-        .then((response) => {
-            const todoRefs = response.data
-            console.log('Todo refs', todoRefs)
-            console.log(`${todoRefs.length} todos found`)
-            // create new query out of todo refs. http://bit.ly/2LG3MLg
-            const getAllTodoDataQuery = todoRefs.map((ref) => {
-                return q.Get(ref)
-            })
-            // then query the refs
-            return client.query(getAllTodoDataQuery).then((ret) => {
-                return {
-                    statusCode: 200,
-                    body: JSON.stringify(ret)
-                }
-            })
-        }).catch((error) => {
-            console.log('error', error)
-            return {
-                statusCode: 400,
-                body: JSON.stringify(error)
-            }
-        })
+    .catch(err => {
+      console.error(
+        'Error: [%s] %s: %s',
+        err.name,
+        err.message,
+        err.errors()[0].description,
+      )
+
+      return {
+        statusCode: 500,
+        body: JSON.stringify(err),
+      }
+    })
 }
--- src/App.js
+++ src/App.js
@@ -1,32 +1,33 @@
-import './App.css';
-import {useEffect, useState} from "react";
+import './App.css'
+import { useEffect, useState } from 'react'
 import axios from 'axios'
 
 const App = () => {
-    const [alerts, setAlerts] = useState([])
+  const [alerts, setAlerts] = useState([])
 
-    const fetchData = async () => {
-        try {
-            const results = await axios('/.netlify/functions/api')
-            console.info(results)
+  const fetchData = async () => {
+    try {
+      const results = await axios('/.netlify/functions/api')
+      console.info(results)
 
-            setAlerts(results?.data)
-        } catch (e) {
-            console.error('ui error', e)
-        }
+      setAlerts(results?.data)
+    } catch (e) {
+      console.error('ui error', e)
     }
+  }
 
+  useEffect(() => {
+    void fetchData()
+  }, [])
 
-    useEffect(() => {
-        void fetchData()
-    }, [])
-
-
-    return (
-        <div className="dashboard">
-            <pre>{JSON.stringify(alerts)}</pre>
-        </div>
-    );
+  return (
+    <>
+      <h2>RMM Webhook Dashboard</h2>
+      <div className="dashboard">
+        <pre>{JSON.stringify(alerts)}</pre>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
