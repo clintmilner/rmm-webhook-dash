386645a8575de5f545b52c70f06b051c980da515
Clint Milner <cmilner@datto.com>
2022-02-20 13:30:24 +0000

    using callback to return status codes to see if netlify works better

--- functions/hook.js
+++ functions/hook.js
@@ -4,14 +4,14 @@
  *   "hostname":"[device_hostname]",
  *   "deviceId":"[device_id]",
  *   "siteId":"[site_id]",
- *   "siteName":"[device_id]",
+ *   "siteName":"[site_name]",
  *   "priority":"moderate",
  *   "msg":"[alert_message]",
  *   "category":"[alert_category]"
  * }
  *
  */
-exports.handler = event => {
+exports.handler = (event, context, callback) => {
   const faunadb = require('faunadb')
   const q = faunadb.query
   const client = new faunadb.Client({
@@ -25,38 +25,39 @@ exports.handler = event => {
 
   // validate the data is good
   if (!hostname || !deviceId || !siteId) {
-    return {
+    return callback(null, {
       statusCode: 400,
       body: 'Error: request does not contain valid details',
-    }
+    })
   }
 
   // successful webhook test
   if (siteId === '[site_id]') {
-    return {
+    return callback(null, {
       statusCode: 200,
       body: 'Webhook test was successful',
-    }
+    })
   }
 
   // only accepting data from our site
   if (siteId !== process.env.ALLOWED_SITE_ID) {
-    return {
+    return callback(null, {
       statusCode: 400,
       body: 'Error: alert data is only accepted from certain sites',
-    }
+    })
   }
 
   const now = Date.now()
   const alert = { ...body, date: now }
 
-
   return client
     .query(q.Create(q.Collection('alerts'), { data: alert }))
-    .then(ret => ({
-      statusCode: 200,
-      body: 'alert created successfully',
-    }))
+    .then(() => {
+      return callback(null, {
+        statusCode: 200,
+        body: 'alert created successfully',
+      })
+    })
     .catch(err => {
       console.error(
         'Error: [%s] %s: %s',
@@ -65,9 +66,9 @@ exports.handler = event => {
         err.errors()[0].description,
       )
 
-      return {
+      return callback(null, {
         statusCode: 500,
         body: JSON.stringify(err),
-      }
+      })
     })
 }
